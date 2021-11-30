e063e8542a849e7c09fd8ee0a5a88218739aea89
Apollo <colin.kenjiro.smith@gmail.com>
2021-11-30 13:52:11 -0500

    finished 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -15,19 +15,27 @@ const sum = function(array) {
   })
 };
 
-console.log(sum([1, 2, 3]))
-
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce(function (previousValue, currentValue) {
+    return previousValue * currentValue
+  })
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  if (number === 0) {
+    return 1
+  }
+  let sum = 1
+	for (let i = 1; number >= i; i++) {
+     sum = sum * i
+  }
+  return sum
 };
+
 
 // Do not edit below this line
 module.exports = {
