72d3744f2b057bd14c165b495c2badfc14a22620
Apollo <colin.kenjiro.smith@gmail.com>
2021-11-30 16:25:05 -0500

    finished 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,29 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const checkPunctuation = function (array) {
+        if (array !== '!' && array !== ' ' && array !== ',' && array !== '.') {
+            return array
+        } else {
+            return
+        }
+    }
+    let lowerCaseString = string.toLowerCase()
+    let myArray = lowerCaseString.split('')
+    let sanitizedArray = myArray.filter(checkPunctuation)
+    let placeholderArray = sanitizedArray.slice()
+    let reversedArray = placeholderArray.reverse()
+    let sanitizedString = sanitizedArray.join("")
+    let reversedString = reversedArray.join("")
+
+    if (reversedString == sanitizedString) {
+        return true
+    } else {
+        return false
+    }
 
 };
+console.log(palindromes('racecar'))
+console.log(palindromes('Racecar'))
+console.log(palindromes('Racecar!! and periods.'))
 
 // Do not edit below this line
 module.exports = palindromes;
