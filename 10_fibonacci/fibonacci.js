cd61787bcbe84538852736471e08353cbd2bfe37
Apollo <colin.kenjiro.smith@gmail.com>
2021-11-30 17:08:32 -0500

    finished 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(input) {
+    if (input < 0) {
+        return "OOPS"
+    }
+    if (typeof(input) === 'string') {
+        input = parseInt(input)
+    }
+    let array = [0, 1, 1]
+    for (let i = 2; i < (input + 2); i++)
 
+        array.push(array[array.length - 1]+array[array.length - 2])
+    return array[input]
 };
+console.log(fibonacci(4))
+console.log(fibonacci(6))
+console.log(fibonacci(-25))
+console.log(fibonacci("5"))
 
 // Do not edit below this line
 module.exports = fibonacci;
